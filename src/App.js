04ca31adc94098e2938fe3db38c19b29d61d979d
ctrachte <cetrachte@hotmail.com>
2020-05-27 15:05:08 -0500

    add  cart and cartItem, cleanup/renaming

--- src/App.js
+++ src/App.js
@@ -1,12 +1,40 @@
-import React from 'react';
-import items from './api/api';
-import Product from './components/Product/product';
+import React, { useState } from 'react';
+// import { Elements, StripeProvider } from 'react-stripe-elements';
+import items from './api/items';
+import Product from './Components/Product/Product';
+import Cart from './Components/Cart/Cart';
+// import CheckoutForm from './components/CheckoutForm/CheckoutForm';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+export default function App() {
+  const [itemsInCart, setItemsInCart] = useState([]);
+
+  const handleAddToCartClick = id => {
+    setItemsInCart(itemsInCart => {
+      const itemInCart = itemsInCart.find(item => item.id === id);
+
+      // if item is already in cart, update the quantity
+      if (itemInCart) {
+        return itemsInCart.map(item => {
+          if (item.id !== id) return item;
+          return { ...itemInCart, quantity: item.quantity + 1 };
+        });
+      }
+
+      // otherwise, add new item to cart
+      const item = items.find(item => item.id === id);
+      return [...itemsInCart, { ...item, quantity: 1 }];
+    });
+  };
+
+  const totalCost = itemsInCart.reduce(
+    (acc, item) => acc + item.price * item.quantity,
+    0
+  );
+
   return (
-<div className="App">
+    <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h1 className="App-header-text">Dreamcast Shop</h1>
@@ -14,12 +42,16 @@ function App() {
       <main className="App-shop">
         <div className="App-products">
           {items.map(item => (
-            <Product key={item.title} title={item.title} price={item.price} />
+            <Product
+              key={item.title}
+              title={item.title}
+              price={item.price}
+              onAddToCartClick={() => handleAddToCartClick(item.id)}
+            />
           ))}
         </div>
+        <Cart itemsInCart={itemsInCart} totalCost={totalCost} />
       </main>
     </div>
   );
 }
-
-export default App;
--- src/components/Product/product.js
+++ src/components/Product/product.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import './product.css';
+import './Product.css';
 
 export default function Product({ onAddToCartClick, price, title }) {
   return (
