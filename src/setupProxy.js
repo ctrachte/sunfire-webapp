8aeca27c1d75dee409610f7a7bcadafc302fa58a
ctrachte <cetrachte@hotmail.com>
2020-06-10 16:24:15 -0500

    revert proxy changes

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,8 +1,7 @@
 const proxy = require('http-proxy-middleware');
-
 module.exports = function(app) {
   app.use(
-    proxy('/.netlify/functions/', {
+    proxy.createProxyMiddleware('/.netlify/functions/', {
       target: 'http://localhost:9000/',
       pathRewrite: {
         '^/\\.netlify/functions': '',
@@ -10,3 +9,4 @@ module.exports = function(app) {
     })
   );
 };
+
