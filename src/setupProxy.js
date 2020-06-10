25638bf4fe3f9dca2aa5c1e6414babb2c3de9e0f
ctrachte <cetrachte@hotmail.com>
2020-06-10 16:21:37 -0500

    update packages, fix proxy

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,7 +1,8 @@
 const proxy = require('http-proxy-middleware');
+
 module.exports = function(app) {
   app.use(
-    proxy.createProxyMiddleware('/.netlify/functions/', {
+    proxy('/.netlify/functions/', {
       target: 'http://localhost:9000/',
       pathRewrite: {
         '^/\\.netlify/functions': '',
