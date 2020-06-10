3940975398031253c2e9eac056a673961426f235
ctrachte <cetrachte@hotmail.com>
2020-06-10 15:44:00 -0500

    fixed proxy bug

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,8 +1,7 @@
 const proxy = require('http-proxy-middleware');
-
 module.exports = function(app) {
   app.use(
-    proxy('/.netlify/functions/', {
+    proxy.createProxyMiddleware('/.netlify/functions/', {
       target: 'http://localhost:9000/',
       pathRewrite: {
         '^/\\.netlify/functions': '',
