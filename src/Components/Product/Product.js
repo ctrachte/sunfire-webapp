b4e313fc8ab4b09b4226599d03b129eef0f74a87
Caleb Trachte <cetrachte@hotmail.com>
2020-11-04 15:19:10 -0600

    adjust colors and separate button styling by class

--- src/Components/Product/Product.js
+++ src/Components/Product/Product.js
@@ -7,7 +7,7 @@ export default function Product({ onAddToCartClick, price, title, qty }) {
       <h2 className="Product-title">{title}</h2>
       <div className="Product-price">${price}</div>
       <div className="Product-qty">{qty} bottle(s) in stock.</div>
-      <button className="big-button Product-buy-button" onClick={onAddToCartClick}>
+      <button className="big-button Product-Buy-Button" onClick={onAddToCartClick}>
         Add to cart
       </button>
     </div>
